7f7377d57399e6d1676ffe6c636af69dd9292f76
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-09-02 20:45:51 +0530

    submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,23 @@ package com.driver;
 
 public class Main {
 
+    public static class Product{
+        public int product(int x, int y) {
+            return x;
+        }
+        public int product(int x, int y, int z) {
+            return x;
+        }
+        public double product(double x, double y) {
+            return x;
+        }
+    }
+
+    public static void main(String[] args){
+        Product p =new Product();
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.0,2.0);
+    }
+
 }
